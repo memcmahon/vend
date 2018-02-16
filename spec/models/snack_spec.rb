dce0d8902f60124c0c00ace9dcc36d09fe1e8c5a
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-02-16 09:57:05 -0700

    Adds model test for analytics

--- spec/models/snack_spec.rb
+++ spec/models/snack_spec.rb
@@ -4,4 +4,12 @@ describe Snack, type: :model do
   describe "Relationships" do
     it { should have_many :machines }
   end
+
+  describe "Analytics" do
+    it "can show price in decimal" do
+      cheetos = Snack.create(name: "Flaming Hot Cheetos", price: 250)
+
+      expect(cheetos.price_to_dollars).to eq(2.5)
+    end
+  end
 end
