fecf64eab8ebe0761869c345a60235c5bae5d3cf
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-02-16 09:58:37 -0700

    Adds model test for analytics

--- spec/models/snack_spec.rb
+++ spec/models/snack_spec.rb
@@ -11,5 +11,13 @@ describe Snack, type: :model do
 
       expect(cheetos.price_to_dollars).to eq(2.5)
     end
+
+    it "can calculate average price" do
+      burger = Snack.create(name: "White Castle Burger", price: 350)
+      poprocks = Snack.create(name: "Pop Rocks", price: 150)
+      cheetos = Snack.create(name: "Flaming Hot Cheetos", price: 250)
+
+      expect(Snack.average_price).to eq(2.5)
+    end
   end
 end
