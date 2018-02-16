96b3edb34e1bba402ea3991d9dccdf9130bf1d67
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-02-16 09:24:28 -0700

    Adds spec for user sees all items in vending machine

--- spec/features/user_can_see_an_individual_vending_machine_spec.rb
+++ spec/features/user_can_see_an_individual_vending_machine_spec.rb
@@ -9,4 +9,19 @@ feature 'When a user visits a vending machine show page' do
 
     expect(page).to have_content("Don's Mixed Drinks Vending Machine")
   end
+
+  scenario 'they see all snacks and price in the vending machine' do
+    owner = Owner.create(name: "Sam's Snacks")
+    dons  = owner.machines.create(location: "Don's Mixed Drinks")
+    burger = dons.snacks.create(name: "White Castle Burger", price: 350)
+    poprocks = dons.snacks.create(name: "Pop Rocks", price: 150)
+    cheetos = dons.snacks.create(name: "Flaming Hot Cheetos", price: 250)
+
+    visit machine_path(dons)
+
+    expect(page).to have content("White Castle Burger: $3.50")
+    expect(page).to have content("Pop Rocks: $1.50")
+    expect(page).to have content("Flaming Hot Cheetos: $2.50")
+  end
+
 end
